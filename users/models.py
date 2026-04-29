dff6c13200a5fa01176d19f2d1a7cf315a9e268e
Maria Mileeva <mileeva.mary@gmail.com>
2026-04-29 22:57:55 +0400

    fix str for User

--- users/models.py
+++ users/models.py
@@ -14,3 +14,6 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
