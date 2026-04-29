1c0d495635224d54a017ec11848fb3b262a64470
Maria Mileeva <mileeva.mary@gmail.com>
2026-04-29 22:50:38 +0400

    fix select options

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
 from .models import Task
+from users.models import User
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].queryset = User.objects.all()
