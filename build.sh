613eabce4e6f7b5d84199dab3edc075d0522b175
Maria Mileeva <mileeva.mary@gmail.com>
2026-04-29 14:56:23 +0400

    debug

--- build.sh
+++ build.sh
@@ -6,10 +6,7 @@ source $HOME/.local/bin/env
 # здесь добавьте все необходимые команды для установки вашего проекта
 # команду установки зависимостей, сборки статики, применения миграций и другие
 
+echo "Folder"
+ls -la
 mkdir logs
 make install
-
-# починка бд
-rm db.sqlite3
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc" -delete
