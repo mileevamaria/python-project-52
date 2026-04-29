9d8ff02129c7ab880f0a5a5b84123bd1c9a05d51
Maria Mileeva <mileeva.mary@gmail.com>
2026-04-29 22:32:40 +0400

    fix user update form

--- users/forms.py
+++ users/forms.py
@@ -37,6 +37,17 @@ class UserRegisterForm(UserCreationForm):
 
 
 class UserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput,
+        required=False
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+        required=False
+    )
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
@@ -45,12 +56,28 @@ class UserUpdateForm(forms.ModelForm):
             'last_name': 'Фамилия',
             'username': FIELD_USERNAME__NAME,
         }
-        help_texts = {
-            'username': '''
-                Обязательное поле. Не более 150 символов. 
-                Только буквы, цифры и символы @/./+/-/_.',
-            '''
-        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+
+        if p1 or p2:
+            if p1 != p2:
+                raise forms.ValidationError('Пароли не совпадают')
+            if len(p1) < 3:
+                raise forms.ValidationError(
+                    'Пароль должен быть не менее 3 символов')
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
 
 
 class UserLoginForm(AuthenticationForm):
